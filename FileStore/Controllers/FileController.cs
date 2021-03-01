3278903dfe5ffd9abf4f363556888ba245e76755
Anders Meidahl <Andersmeidahl@gmail.com>
2021-03-01 21:02:07 +0100

    Added file-store

---
+++ FileStore/Controllers/FileController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.WebEncoders.Testing;
+
+namespace FileStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        private const string FileName = "test.txt";
+
+        [HttpGet]
+        public string Get()
+        {
+            if (!System.IO.File.Exists(FileName)) return "";
+            using var sr = System.IO.File.OpenText(FileName);
+            return sr.ReadToEnd();
+        }
+
+        [HttpPost]
+        public IActionResult Post(string input)
+        {
+            using var streamWriter = System.IO.File.CreateText(FileName);
+            streamWriter.WriteLine(input);
+            return Ok();
+        }
+    }
+}
