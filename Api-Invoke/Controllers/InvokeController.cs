b9c5d9ffc748821f8703ad7adb37556f41e23c27
Anders Meidahl <Andersmeidahl@gmail.com>
2021-03-01 20:42:42 +0100

    Added api-invoke project

---
+++ Api-Invoke/Controllers/InvokeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Api_Invoke.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvokeController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            var httpClient = new HttpClient();
+            var httpResponseMessage = await httpClient.GetStringAsync("http://api-service:80/api/environment");
+            return httpResponseMessage;
+        }
+    }
+}
